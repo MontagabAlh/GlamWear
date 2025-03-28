dc9c16b52ca7159947be35d2d5463221fe8f0bc1
montagab <montagab@qubefyn.com>
2025-03-28 22:28:26 +0300

    edit dashboard

--- src/app/dashboard/products/[id]/page.tsx
+++ src/app/dashboard/products/[id]/page.tsx
@@ -1,26 +1,28 @@
 import EditForm from '@/components/Pages/Admin/products/EditForm';
 import { prisma } from '@/lib/prisma'
 import { notFound } from 'next/navigation';
-import React from 'react'
+import React from 'react';
 
 async function getData(productId: string) {
     const data = await prisma.product.findUnique({
         where: {
             id: productId,
         },
-    })
+    });
+
     if (!data) {
-        return notFound();
+        notFound(); // هذا سيعرض صفحة 404
     }
 
     return data;
 }
-export default async function EditProduct({ params }: { params: { id: string } }) {
 
+export default async function EditProduct({ params }: { params: { id: string } }) {
     const data = await getData(params.id);
+
     return (
         <div>
-            <EditForm data={data}/>
+            <EditForm data={data} />
         </div>
-    )
+    );
 }

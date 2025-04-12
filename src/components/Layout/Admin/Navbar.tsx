eed290940a7eb6fd659062843f7812a8526e546c
montagab <montagab@qubefyn.com>
2025-04-12 18:47:47 +0300

    add icons

--- src/components/Layout/Admin/Navbar.tsx
+++ src/components/Layout/Admin/Navbar.tsx
@@ -1,9 +1,19 @@
+/* eslint-disable jsx-a11y/alt-text */
 "use client"
-import { DashbordLinks } from '@/utils/Links'
+
 import { cn } from '@/lib/utils'
+import { Image, LayoutDashboard, Package, ShoppingBag } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+
+
+export const DashbordLinks = [
+  { name: "Dashboard", Link: "/dashboard", icon: <LayoutDashboard className='h-4 w-4' /> },
+  { name: "Orders", Link: "/dashboard/orders", icon: <ShoppingBag className='h-4 w-4' /> },
+  { name: "Products", Link: "/dashboard/products", icon: <Package className='h-4 w-4' /> },
+  { name: "Banners", Link: "/dashboard/banners", icon: <Image className='h-4 w-4' /> },
+]
 
 
 export default function Navbar() {
@@ -17,9 +27,11 @@ export default function Navbar() {
       </Link>
       <div className='flex flex-col md:flex-row  md:items-center justify-center gap-4 mt-3 md:mt-auto'>
         {DashbordLinks.map((link, index) => (
-          <Link key={index} href={link.Link} className={cn(link.Link === pathname ? 'text-foreground bg-muted' : 'text-muted-foreground hover:text-foreground hover:bg-muted ', "transition-all duration-300 ease-in-out group p-2 font-medium rounded-md")}>
-            
-              {link.name}
+          <Link key={index} href={link.Link} className={cn(link.Link === pathname ? 'text-foreground bg-muted' : 'text-muted-foreground hover:text-foreground hover:bg-muted ', "transition-all duration-300 ease-in-out group p-2 font-medium rounded-md flex items-center gap-2")}>
+            <span>
+              {link.icon}
+            </span>
+            {link.name}
           </Link>
         ))}
       </div>
--- src/components/Layout/Web/NavbarLinks.tsx
+++ src/components/Layout/Web/NavbarLinks.tsx
@@ -1,9 +1,17 @@
 "use client"
-import { StoreLinks } from '@/utils/Links'
 import { cn } from '@/lib/utils'
+import { Home, Shirt, ShoppingBag } from 'lucide-react'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
+
+export const StoreLinks = [
+  { name: "Home", Link: "/", icon: <Home className='h-4 w-4' /> },
+  { name: "Shop", Link: "/shop", icon: <ShoppingBag className='h-4 w-4' /> },
+  { name: "Men", Link: "/shop/c/men", icon: <Shirt className='h-4 w-4' /> },
+  { name: "Women", Link: "/shop/c/women", icon: <Shirt className='h-4 w-4' /> },
+  { name: "Kids", Link: "/shop/c/kids", icon: <Shirt className='h-4 w-4' /> },
+]
 
 export default function NavbarLinks() {
   const pathname = usePathname();
@@ -16,8 +24,11 @@ export default function NavbarLinks() {
       </Link>
       <div className='font-medium flex flex-col md:flex-row justify-center  md:items-center gap-x-2 mt-3 md:mt-auto md:ml-5 md:item-center md:gap-5 md:text-sm lg:gap-6'>
         {StoreLinks.map((link, index) => (
-          <Link key={index} href={link.Link} className={cn(link.Link === pathname ? 'text-foreground bg-muted' : 'text-muted-foreground hover:text-foreground hover:bg-muted ', "transition-all duration-300 ease-in-out group p-2 font-medium rounded-md")}>
-              {link.name}
+          <Link key={index} href={link.Link} className={cn(link.Link === pathname ? 'text-foreground bg-muted' : 'text-muted-foreground hover:text-foreground hover:bg-muted ', "transition-all duration-300 ease-in-out group p-2 font-medium rounded-md flex items-center gap-2")}>
+            <span>
+              {link.icon}
+            </span>
+            {link.name}
           </Link>
         ))}
       </div>
